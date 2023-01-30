4317370fd39705c8218b931a98d5b5fcf092ee12
yyaremkiv <y.yaremkiv@gmail.com>
2023-01-30 14:50:07 +0200

    task2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
 
-const router = express.Router()
+const {
+  addContactValidation,
+} = require("../../middlewares/validationMiddleware");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+router.get("/", listContacts);
+router.get("/:id", getContactById);
+router.post("/", addContactValidation, addContact);
+router.put("/:id", addContactValidation, updateContact);
+router.delete("/:id", removeContact);
+module.exports = router;
